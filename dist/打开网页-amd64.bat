3fa1ae0e96305370d53b45421675b4615bf2bdfe
neko <1198565052@qq.com>
2026-07-22 23:50:22 +0800

    提交启动脚本 bat

---
+++ dist/打开网页-amd64.bat
@@ -0,0 +1,8 @@
+@echo off
+rem Start the extractor service if it is not listening on 8080, then open the page
+netstat -ano | findstr /C:":8080 " | findstr /C:"LISTENING" >nul
+if errorlevel 1 (
+    start "" /min "%~dp0N0vaDesktop-Extractor-amd64.exe"
+    ping 127.0.0.1 -n 3 >nul
+)
+start "" http://127.0.0.1:8080
